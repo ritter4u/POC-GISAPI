0ad352b8eac02d75afc921e5d4f4439181a30b27
Keunreol Park <ritter4u@gmail.com>
2022-09-23 18:53:36 +0900

    Init

---
+++ src/test/kotlin/io/github/ritter4u/POCGISAPI/PocGisapiApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.github.ritter4u.POCGISAPI
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PocGisapiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
