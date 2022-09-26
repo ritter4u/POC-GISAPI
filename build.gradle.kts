115f943eae9437bae9dd87903e0184af04de5b02
Keunreol Park <ritter4u@gmail.com>
2022-09-27 03:03:53 +0900

    Setting fix

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,4 @@
+import org.gradle.internal.impldep.org.junit.experimental.categories.Categories.CategoryFilter.exclude
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
@@ -19,6 +20,18 @@ repositories {
 	maven { url = uri("https://maven.pkg.jetbrains.space/data2viz/p/maven/public") }
 }
 
+configurations{
+	all {
+		exclude(group = "*", module = "spring-boot-starter-logging")
+		exclude(group = "*", module = "ch.qos.*")
+
+	}
+//	implementation {
+//		resolutionStrategy.failOnVersionConflict()
+//	},
+
+}
+
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-hateoas")
@@ -28,18 +41,24 @@ dependencies {
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-	developmentOnly("org.springframework.boot:spring-boot-devtools")
 
 	implementation("io.springfox:springfox-boot-starter:3.0.0")
 	implementation("io.springfox:springfox-swagger2:3.0.0")
 	implementation("io.springfox:springfox-swagger-ui:3.0.0")
 
-	implementation("com.h2database:h2")
-	implementation("org.postgresql:postgresql")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	// https://mvnrepository.com/artifact/org.hibernate/hibernate-spatial
+	implementation("org.hibernate:hibernate-spatial:6.1.3.Final")
+	implementation("com.h2database:h2:2.1.214")
+	implementation("org.postgresql:postgresql:42.5.0")
+	implementation("net.postgis:postgis-jdbc:2021.1.0")
+	implementation("org.orbisgis.data:h2gis:2.0.0")
 
 	//https://github.com/data2viz/geojson-kotlin
 	implementation("io.data2viz.geojson:core:0.6.4")
+
+	implementation("io.github.dellisd.spatialk:geojson:0.2.1")
+//	developmentOnly("org.springframework.boot:spring-boot-devtools")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
 tasks.withType<KotlinCompile> {
